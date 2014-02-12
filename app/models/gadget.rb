b6693dc6adfb57115555bd0fb2e76e409f5ab967
Alexey Artamonov <i@useruby.com>
2014-02-12 20:26:36 +0800

    changed postgesql to sqlite3

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,5 +1,5 @@
 class Gadget < ActiveRecord::Base
-  scope :filter_by_name, ->(name){where("name ILIKE '#{name}%'")}
+  scope :filter_by_name, ->(name){where("name LIKE '#{name}%'")}
 
   validates_presence_of :name
 
